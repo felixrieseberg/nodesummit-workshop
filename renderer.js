ef346efda9b31565e90e6a2c006183d316869827
Felix Rieseberg <felix@felixrieseberg.com>
2018-07-27 14:07:12 -0700

    :wrench: Contexts, huh?!

--- renderer.js
+++ renderer.js
@@ -1,1 +1,14 @@
-console.log('👋')
+const loader = require('monaco-loader')
+const $ = document.querySelector.bind(document)
+
+let editor
+
+loader().then((monaco) => {
+  const div = $('#container')
+
+  editor = monaco.editor.create(div, {
+    language: 'javascript',
+    theme: 'vs-dark',
+    automaticLayout: true
+  })
+})
