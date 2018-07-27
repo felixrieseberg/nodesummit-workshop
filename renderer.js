6dacbde9ba1bb1b874d513a3332cec74f6a2103c
Felix Rieseberg <felix@felixrieseberg.com>
2018-07-27 14:39:35 -0700

    :wrench: Open a file

--- renderer.js
+++ renderer.js
@@ -1,16 +1,22 @@
+const fs = require('fs')
 const { ipcRenderer } = require('electron')
 const loader = require('monaco-loader')
 const $ = document.querySelector.bind(document)
 
-ipcRenderer.on('open-file', (...args) => {
-  console.log(args)
-})
-
 let editor
+
+ipcRenderer.on('open-file', (e, filePath) => openFile(filePath))
+
+function openFile(filePath) {
+  const content = fs.readFileSync(filePath, 'utf-8');
+  editor.setValue(content)
+}
 
 loader().then((monaco) => {
   const div = $('#container')
 
+  // https://microsoft.github.io/monaco-editor/api/interfaces/
+  // monaco.editor.istandalonecodeeditor.html
   editor = monaco.editor.create(div, {
     language: 'javascript',
     theme: 'vs-dark',
